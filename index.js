77efe7861a9c3a57037cf9a392db1f327b763b0c
Ititriin <triin.tajur@vikk.ee>
2021-10-28 18:59:29 +0300

    deletes too fast

--- index.js
+++ index.js
@@ -6,39 +6,37 @@ async function script() {
 
   // 1. Open the webpage. Log its title
   await driver.get("https://www.weekendshoes.ee/naistele/saapad.html");
-  console.log("opening the web page");
   const titleTag = await driver.getTitle();
   console.log("getting page title:", titleTag);
 
   // 2. Locate and choose item
   await driver.findElement(By.xpath('//*[@id="amasty-shopby-product-list"]/div[3]/ol/li[69]/div/a/picture/img')).click();
-  await driver.findElement(By.className('product-options-wrapper')).click()
-  await driver.manage().setTimeouts({ implicit: 13000 });
-  await driver.findElement(By.xpath('//*[@id="product-options-wrapper"]/div/div/div/div/div[3]/div/ul/li[2]/div[1]')).click()
- 
+  await driver.findElement(By.className("product-options-wrapper")).click();
+  await driver.manage().setTimeouts({ implicit: 20000 });
+  await driver.findElement(By.xpath('//*[@id="product-options-wrapper"]/div/div/div/div/div[3]/div/ul/li[2]/div[1]')).click();
+
   // 3. Add it to the wishlist
   await driver.findElement(By.xpath('//*[@id="maincontent"]/div[2]/div/div[3]/div[1]/a')).click();
   await driver.manage().setTimeouts({ implicit: 10000 });
 
   // 4. Open item via the wishlist
   await driver.manage().setTimeouts({ implicit: 10000 });
-  await driver.findElement(By.id('wishlist-link-mobile')).click();
-  await driver.findElement(By.className('action primary go-to-wishlist')).click();
+  await driver.findElement(By.id("wishlist-link-mobile")).click();
+  await driver.findElement(By.className("action primary go-to-wishlist")).click();
 
   // 5. Add item into shopping cart
   await driver.findElement(By.xpath('//*[@id="wishlist-view-form"]/div[2]/div[1]/button[3]')).click();
 
   // 6. Open shopping cart
-  await driver.findElement(By.className('action viewcart')).click();
+  await driver.findElement(By.className("action viewcart")).click();
 
   // 7. Change item quantity, increasing by 1
-  await driver.manage().setTimeouts({ implicit: 12000 });
-  await driver.findElement(By.className('action qty increase-qty increase-item-qty-btn')).click();
-
-  //increase-cart-qty-btn-5611753
+  await driver.manage().setTimeouts({ implicit: 20000 });
+  await driver.findElement(By.className("action qty increase-qty increase-item-qty-btn")).click();
 
   // 8. Remove all items from the shopping cart
-  //await driver.findElement(By.id(`//*[@id="shopping-cart-table"]/tbody/tr[1]/td[1]/div/a"]`)).click();
+  await driver.manage().setTimeouts({ implicit: 200000 });
+  //await driver.findElement(By.xpath('//*[@id="shopping-cart-table"]/tbody/tr[1]/td[1]/div/a')).click();
 
   // 9. Search for item "Jope"
   //const itemSearch = "Jope";
@@ -49,5 +47,4 @@ async function script() {
   //await driver.findElement(By.id(`//*[@id="sorter"]/option[1]`)).click();
 
 }
-
-script();
+script()
