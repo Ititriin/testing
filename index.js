9e4d21c1c0000c912db83b846022d2e067848082
Ititriin <triin.tajur@vikk.ee>
2021-10-28 10:30:43 +0300

    initial

---
+++ index.js
@@ -0,0 +1,12 @@
+//Steps to test
+
+// 1. Open the webpage. Log it's title
+// 2. Locate item
+// 3. Add one item to the wishlist
+// 4. Open item via the wishlist
+// 5. Add item into shopping cart
+// 6. Open shopping cart
+// 7. Change item quantity, increasing by 1
+// 8. Remove all items from the shopping cart
+// 9. Search for item "Jope"
+// 10. Filter search results by popularity
